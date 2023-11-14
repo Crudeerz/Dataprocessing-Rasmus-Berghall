c4b6c1e70e2af1482a4d5158864754e9fc53627e
Crudeerz <rassefhc@yahoo.se>
2023-11-14 15:00:12 +0100

    L018_Dash_intro

--- Code-Alongs/L018_Dash_intro.py
+++ Code-Alongs/L018_Dash_intro.py
@@ -10,27 +10,36 @@ counter = 0
 app = Dash(__name__)
 
 
-my_H1 = html.H1(style={"fontFamily":"helvetica"},children="My dash application")
-my_H2 = html.H2(id="my-h2", children="More info...")
-my_dropdown = dcc.Dropdown(id="my-dropdown", options=df["year"].unique(), value=2007, style={"width":"200px"})
-my_graph = dcc.Graph(id="my-graph", figure=px.strip(df.query("year == 2007"), x="lifeExp", color="continent"))
-my_button = html.Button("Click here!")
+# my_H1 = html.H1(style={"fontFamily":"helvetica"},children="My dash application")
+# my_H2 = html.H2(id="my-h2", children="More info...")
+# my_dropdown = dcc.Dropdown(id="my-dropdown", options=df["year"].unique(), value=2007, style={"width":"200px"})
+# my_graph = dcc.Graph(id="my-graph", figure=px.strip(df.query("year == 2007"), x="lifeExp", color="continent"))
+# my_button = html.Button("Click here!")
 
-app.layout = html.Div(style={"backgroundColor": "white"},children=[my_H1, my_H2, my_dropdown, my_graph, my_button]) # <h1>My dash app.</h1>
+
+# better practice to get structure:
+
+app.layout = html.Div(style={"backgroundColor": "white"},children=[
+    html.H1(id="my-h1", style={"fontFamily":"helvetica"},children="My dash application"), 
+    html.H2(id="my-h2", children="More info..."),
+    dcc.Dropdown(id="my-dropdown", options=df["year"].unique(), value=2007, style={"width":"200px"}),
+    dcc.Graph(id="my-graph", figure=px.strip(df.query("year == 2007"), x="lifeExp", color="continent")),
+    html.Button(id="my-button", children="Click here!")
+    ]) # <h1>My dash app.</h1>
 
 
 @callback(
-        Output(my_H2, component_property="children"),
-        Output(my_graph, component_property="figure"),
-        Input(my_dropdown, component_property="value")
+        Output("my-h2", component_property="children"),
+        Output("my-graph", component_property="figure"),
+        Input("my-dropdown", component_property="value")
 )
 def select_year(year):
     return f"{year}", px.strip(df.query("year == @year"), x="lifeExp", color="continent")
 
 
 @callback(
-    Output(my_H1, "children"),
-    Input(my_button, "n_clicks")
+    Output("my-h1", "children"),
+    Input("my-button", "n_clicks")
 )
 def button_clicked(n):
     global counter
